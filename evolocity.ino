3b505bb58db86b7d278260f900850037bd1b7fda
Mike Frampton <michael.frampton@pg.canterbury.ac.nz>
2015-10-30 09:26:57 +1300

    skeleton for evolocity code

---
+++ evolocity.ino
@@ -0,0 +1,15 @@
+#include <Canbus.h>
+#include <defaults.h>
+#include <global.h>
+#include <mcp2515.h>
+#include <mcp2515_defs.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
